606a677104638dbbc77dd8d39e07bc5c08693e2f
mapo9 <mark.poslter@qbic.uni-tuebingen.de>
2025-11-24 07:55:05 +0100

    make reproducible

--- t2pmhc/models/t2pmhc_gcn.py
+++ t2pmhc/models/t2pmhc_gcn.py
@@ -37,6 +37,8 @@ def set_seed(seed=42):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 set_seed(42)
 
