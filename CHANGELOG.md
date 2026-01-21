ba0bd6bc296c423b6dbbfc4788708d0eba0a8553
mapo9 <mark.polster@qbic.uni-tuebingen.de>
2026-01-21 11:16:33 +0100

    update changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+
+## [1.0.0] - Ionic Hleb (2025-21-01)
+
+### Added
+
+- Initial release of t2pmhc
+- Creation of t2pmhc graphs
+- Training of t2pmhc-GCN and t2pmhc-GAT
+- Prediction of samples using both variants
+- Dockerfile for v1.0.0
+- Documentation
+
+### Fixed
--- setup.py
+++ setup.py
@@ -3,16 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name="t2pmhc",
     author="Mark Polster",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     include_package_data=True,
     package_data={
         "t2pmhc": ["data/**/*",
                    "utils/*.json",]
     },
-    install_requires=[
-        # TODO move requirementx.txt here
-    ],
     entry_points={
         "console_scripts": [
             "t2pmhc = t2pmhc.__main__:run_t2pmhc",
